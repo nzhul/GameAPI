a23b9dd14a660627f1ded631445398f2194faef3
Dobromir Ivanov <dobromirivanov1@gmail.com>
2015-02-15 23:40:17 +0200

    Phaser test

--- Application/Application.Web/Game/Models/game.js
+++ Application/Application.Web/Game/Models/game.js
@@ -4,27 +4,46 @@ define([
     'use strict';
 
     function Game(container) {
-        this.container = container
-        console.log('Making the Game');
+        this.container = container;
+        this.game = new Phaser.Game(800, 600, Phaser.AUTO, this.container);
+        this.Boot = function () {};
+
+        this.Boot.prototype = {
+            preload: function () {
+                this.load.image('logo', 'Game/Graphics/phaser.png');
+                this.load.image('preloadbar', 'assets/images/preloader-bar.png');
+            },
+            create: function () {
+                // Loading screen will have a white background
+                this.game.stage.backgroundColor = '#ccc';
+
+                // scaling options
+                this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+                this.scale.minWidth = 240;
+                this.scale.minHeight = 170;
+                this.scale.maxWidth = 2880;
+                this.scale.minHeight = 1920;
+
+                // have the game centered horizontally
+                this.scale.pageAlignHorizontally = true;
+
+                // screen size will be set automatically
+                this.scale.setScreenSize(true);
+
+                // physics system for movement
+                this.game.physics.startSystem(Phaser.Physics.ARCADE);
+
+                this.state.start('Preload');
+            }
+        };
+
+        this.game.state.add('Boot', this.Boot);
+        this.game.state.start('Boot');
     }
 
     Game.prototype = {
-        constructor: Game,
+        Boot: function () {
 
-        start: function() {
-            this.game = new Phaser.Game(800, 600, Phaser.AUTO, this.container, {
-                preload: this.preload,
-                create: this.create
-            });
-        },
-
-        preload: function() {
-            this.game.load.image('logo', 'Game/Graphics/phaser.png');
-        },
-
-        create: function() {
-            var logo = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'logo');
-            logo.anchor.setTo(0.5, 0.5);
         }
     };
 
--- Application/Application.Web/GameAngularPhaser/scripts/game/main.js
+++ Application/Application.Web/GameAngularPhaser/scripts/game/main.js
@@ -3,4 +3,13 @@
  */
 window.createGame = function (scope, players, mapId, injector) {
   // Create our phaser game
+
+    var height = parseInt(ele.css('height'), 10),
+        width = parseInt(ele.css('width'), 10);
+
+    var game = new Phaser.Game(width, height, Phaser.AUTO, 'gameCanvas');
+
+    var Game = require('./states'),
+        states = Game.States;
+
 };
