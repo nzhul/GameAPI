38035d49b4830fcf51b4365031779ed43b5e85ff
nzhul <dobromirivanov1@gmail.com>
2015-02-19 23:04:16 +0200

    Simple Space Game Completed

--- Application/Application.Web/Game/States/GameState.js
+++ Application/Application.Web/Game/States/GameState.js
@@ -30,21 +30,30 @@ define([], function () {
                 this.game.camera.follow(this.player);
 
                 // Generate game elements
+                this.generateCollectables();
                 this.generateAsteroids();
 
                 //sounds
                 this.explosionSound = this.game.add.audio('explosion');
                 this.collectSound = this.game.add.audio('collect');
+
+                // Show score
+                this.showLabels();
             },
             update: function () {
                 if(this.game.input.activePointer.justPressed()){
 
                     // move on the direction of the input
-                    this.game.physics.arcade.moveToPointer(this.player, this.playerSpeed);
+                    if (this.player.exists) {
+                        this.game.physics.arcade.moveToPointer(this.player, this.playerSpeed);
+                    }
                 }
 
                 // Collision between player and asteroids
                 this.game.physics.arcade.collide(this.player, this.asteroids, this.hitAsteroid, null, this);
+
+                // Collision ( overlapping ) between player and collectables
+                this.game.physics.arcade.overlap(this.player, this.collectables, this.collect, null, this);
             },
             generateAsteroids: function () {
                 this.asteroids = this.game.add.group();
@@ -70,15 +79,61 @@ define([], function () {
                     asteroid.body.collideWorldBounds = true;
                 }
             },
+            generateCollectables: function () {
+                this.collectables = this.game.add.group();
+
+                // Enable physics in them
+                this.collectables.enableBody = true;
+                this.collectables.physicsBodyType = Phaser.Physics.ARCADE;
+
+                // Phaser random number generator
+                var collectablesCount = this.game.rnd.integerInRange(100, 150);
+                var collectable;
+
+                for (var i = 0; i < collectablesCount; i++) {
+                    collectable = this.collectables.create(this.game.world.randomX, this.game.world.randomY, 'power');
+                    collectable.animations.add('fly', [0, 1, 2, 3], 5, true);
+                    collectable.animations.play('fly');
+                }
+            },
             hitAsteroid: function (player, asteroid) {
                 // play explosion sound
                 this.explosionSound.play();
 
-                // player explosion will be added here
-
+                // Player explosion - particle
+                var emitter = this.game.add.emitter(this.player.x, this.player.y, 100);
+                emitter.makeParticles('playerParticle');
+                emitter.minParticleSpeed.setTo(-200, -200);
+                emitter.maxParticleSpeed.setTo(200, 200);
+                emitter.gravity = 0;
+                emitter.start(true, 1000, null, 100);
                 this.player.destroy();
 
                 this.game.time.events.add(800, this.gameOver, this);
+            },
+            collect: function (player, collectable) {
+
+                // Play collect sound
+                this.collectSound.play();
+
+                // Update score
+                this.playerScore++;
+                console.log("Player score: " + this.playerScore);
+                this.scoreLabel.text = this.playerScore;
+
+                //remove sprite
+                collectable.destroy();
+            },
+            showLabels: function () {
+                // Score text
+                var text = "0";
+                var style = {font: "20px Arial", fill: "#fff", align: "center"};
+                this.scoreLabel = this.game.add.text(this.game.width-50, this.game.height-50, text, style);
+                this.scoreLabel.fixedToCamera = true;
+            },
+            gameOver: function () {
+                // Pass the score as a parameter
+                this.game.state.start('MainMenuState', true, false, this.playerScore);
             }
         };
 
