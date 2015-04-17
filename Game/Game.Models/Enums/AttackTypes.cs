7a4236b39cff3527ee33f26f8ccc4262f2ea544a
nzhul <dobromirivanov1@gmail.com>
2015-04-17 11:33:37 +0300

    Units start

---
+++ Game/Game.Models/Enums/AttackTypes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game.Models.Enums
+{
+    public enum AttackTypes
+    {
+        Melee,
+        Range,
+        Cast
+    }
+}
---
+++ Game/Game.Models/Units/Human/Swordsman.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game.Models.Units.Human
+{
+    public class Swordsman : IUnit
+    {
+        const int minDamage = 1;
+        const int maxDamage = 3;
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public Enums.AttackTypes AttackTypes { get; set; }
+
+        public int Attack { get; set; }
+
+        public int Defense { get; set; }
+
+        public int MinDamage { get; set; }
+
+        public int MaxDamage { get; set; }
+
+        public int Health { get; set; }
+
+        public int Speed { get; set; }
+
+        public int Size { get; set; }
+
+        public int Growth { get; set; }
+
+        public string Description { get; set; }
+
+        public int StackSize { get; set; }
+
+    }
+}
